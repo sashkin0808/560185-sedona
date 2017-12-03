c4e09425c35541d6e4c843ad223130d06e8d22df
sashkin0808 <33091916+sashkin0808@users.noreply.github.com>
2017-12-03 20:25:00 +0300

    исправление скрипта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var link = document.querySelector(".search-button");
 var popup = document.querySelector(".search-form");
 var checkin = popup.querySelector("[name=checkin-date]");
+popup.classList.remove("modal-show");
 link.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-show");
