bcc979b75da75bc723f333a9723de3660d646f92
sashkin0808 <33091916+sashkin0808@users.noreply.github.com>
2017-12-06 22:37:57 +0300

    исправление

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 var link = document.querySelector(".search-button");
 var popup = document.querySelector(".search-form");
 var checkin = popup.querySelector("[name=checkin-date]");
+var checkout = popup.querySelector("[name=checkout-date]");
 popup.classList.remove("modal-show");
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("modal-show");
-    setTimeout(function() {
-    	checkin.focus();
-    }, 400);
+	evt.preventDefault();
+	popup.classList.toggle("modal-show");
+	popup.classList.remove("modal-error");
+	setTimeout(function() {
+		checkin.focus();
+	}, 400);
+});
+popup.addEventListener("submit", function (evt) {
+	if (!checkin.value || !checkout.value || !adults.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal-error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal-error");
+	}
 });
